17ca0e0fcc01377a9a36c445f86ba444dbddc17f
Joseph Richardson <JoeHCQ1@protonmail.com>
2022-02-18 23:26:55 -0500

    gates: added last few methods

--- simulator/gate.cpp
+++ simulator/gate.cpp
@@ -11,3 +11,15 @@ void Gate::process()
 {
     output_junction_ptr->state = logicFunction(Input::to_values(input_junction_ptrs));
 }
+
+std::vector<uint32_t> Gate::get_input_j_ids() const {
+    std::vector<uint32_t> ret;
+    for (uint32_t i = 0; i < input_junction_ptrs.size(); i++) {
+        ret.push_back(input_junction_ptrs[i]->index_id);
+    }
+    return ret;
+}
+
+uint32_t Gate::get_output_j_id() const {
+    return output_junction_ptr->index_id;
+}
